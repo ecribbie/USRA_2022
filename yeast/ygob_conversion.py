8b981edd72b39e7859716aabac7dd2dc665ea3c0
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-05-16 14:44:06 -0700

    Update ygob_conversion.py

--- yeast/ygob_conversion.py
+++ yeast/ygob_conversion.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python
 
-## get txt file of species genomes
-# file=input('file name: ')
-# #read in that txt file to raw
-# f=open('file','r')
-# raw=f.readlines()
-# f.close()
-
-# #output file to check
-# print(raw)
-
-
-
-
-#testing bash
-test=input('some input to test please :)    :')
-print(test)
-print("i ran")
-
+#will want to have a way to input desired file but for now using directly ygob_testfile.txt
+f.open("ygob_testfile.txt")
+raw=f.readlines()
+f.close()
+print(raw[:10])
