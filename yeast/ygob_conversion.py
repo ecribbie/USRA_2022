6807cfa2f2bfb2fe4c36eb34e0500222f90d1740
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-05-16 14:38:01 -0700

    Update ygob_conversion.py

--- yeast/ygob_conversion.py
+++ yeast/ygob_conversion.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
-# get txt file of species genomes
-file=input('file name: ')
-#read in that txt file to raw
-f=open('file','r')
-raw=f.readlines()
-f.close()
+## get txt file of species genomes
+# file=input('file name: ')
+# #read in that txt file to raw
+# f=open('file','r')
+# raw=f.readlines()
+# f.close()
 
-#output file to check
-print(raw)
+# #output file to check
+# print(raw)
+
+
+
+
+#testing bash
+test=input('some input to test please :)    :')
+print(test)
 print("i ran")
 
