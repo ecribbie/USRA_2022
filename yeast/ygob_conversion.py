25e5a964545c4d4fd4c43d950dedea44dc966695
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-05-16 14:45:14 -0700

    Update ygob_conversion.py

--- yeast/ygob_conversion.py
+++ yeast/ygob_conversion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #will want to have a way to input desired file but for now using directly ygob_testfile.txt
-f.open("ygob_testfile.txt")
+f=open("ygob_testfile.txt",'r')
 raw=f.readlines()
 f.close()
 print(raw[:10])
