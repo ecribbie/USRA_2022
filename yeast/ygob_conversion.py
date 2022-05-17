4ce688d18ecdc652ccbadce425caee6d82a997a9
ecribbie <ecribbie@sfu.ca>
2022-05-17 14:53:55 -0700

    update

--- yeast/ygob_conversion.py
+++ yeast/ygob_conversion.py
@@ -5,17 +5,28 @@ f=open("ygob_testfile.txt",'r')
 raw=f.readlines()
 f.close()
 
-gene=[None]*len(raw)
-direction=[None]*len(raw)
-similarities=[None]*len(raw)
+#gene=[None]*len(raw)
+#direction=[None]*len(raw)
+#similarities=[None]*len(raw)
+#for i in range(len(raw)):
+#  x=repr(raw[i]).split('\\t')
+#  gene[i]=x[0]
+#  direction[i]=x[1]
+#  similarities[i]=x[8]
+#print(gene[:10])
+#print(direction[:10])
+#print(similarities[:10])
+gene={}
 for i in range(len(raw)):
   x=repr(raw[i]).split('\\t')
-  gene[i]=x[0]
-  direction[i]=x[1]
-  similarities[i]=x[8]
-print(gene[:10])
-print(direction[:10])
-print(similarities[:10])
+  gene[x[0]]={}
+  gene[x[0]]['dir']=x[1]
+  gene[x[0]]['sim']=x[8]
+  gene[x[0]]['start']=x[2]
+  gene[x[0]]['end']=x[3]
 
+print(gene[repr(raw[0]).split('\\t')[0]])
+print('---------------------------------------------')
+print(gene[repr(raw[1]).split('\\t')[0]])
 
   
