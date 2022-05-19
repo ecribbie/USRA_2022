12e5a76cce8f372a233325f8ad644ac14d82ec92
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-05-19 10:32:01 -0700

    Update genome_tab_to_dictionary.py

--- YGOB/genome_tab_to_dictionary.py
+++ YGOB/genome_tab_to_dictionary.py
@@ -2,7 +2,7 @@
 
 #will want to have a way to input desired file but for now using directly ygob_testfile.txt
 
-f=open("Cglabrata_genome.txt",'r')
+f=open("species_genome_tabs/Tphaffii_genome.txt",'r')
 raw=f.readlines()
 f.close()
 
