1e756152f24a0689c06f61e55e2969a19a14e451
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-05-19 10:45:11 -0700

    Update genome_tab_to_dictionary.py

--- YGOB/genome_tab_to_dictionary.py
+++ YGOB/genome_tab_to_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import re
+
 #will want to have a way to input desired file but for now using directly ygob_testfile.txt
 
 f=open("species_genome_tabs/Tphaffii_genome.txt",'r')
@@ -8,17 +10,28 @@ f.close()
 
 #Create nested dictionary for the species genes then direction (dir), similarity sentence (sim) and start and end coordinates (start) (end)
 #This is for the basic layout of species genomes see link:ygob.ucd.ie > genome sequences > README > section (2) the delimiter between columns when copied to txt is \\t
-gene={}
+species={}
 for i in range(len(raw)):
   x=repr(raw[i]).split('\\t')
-  gene[x[0]]={}
-  gene[x[0]]['dir']=x[1]
-  gene[x[0]]['sim']=x[8]
-  gene[x[0]]['start']=x[2]
-  gene[x[0]]['end']=x[3]
+  species[x[0]]={}
+  species[x[0]]['dir']=x[1]
+  species[x[0]]['sim']=x[8]
+  species[x[0]]['start']=x[2]
+  species[x[0]]['end']=x[3]
 
-print(gene[repr(raw[0]).split('\\t')[0]])
+print(species[repr(raw[0]).split('\\t')[0]])
 print('---------------------------------------------')
-print(gene[repr(raw[1]).split('\\t')[0]])
+print(species[repr(raw[6]).split('\\t')[0]])
 
+if author=="Gordon":
+  for gene in species:
+    if len(species[gene]['sim'].split(" "))==1:
+      species[gene]['match']=species[gene]['sim']
+    elif re.search("cerevisia",species[gene]['sim'].split(" ")):
+      ind=species[gene]['sim'].split(" ").index("cerivisia")+1
+      species[gene]['match']=species[gene]['sim'].split(" ")[ind]
+    else:
+      species[gene]['match']=None
+print(species[repr(raw[0]).split('\\t')[0]])
+print(species[repr(raw[6]).split('\\t')[0]])
   
