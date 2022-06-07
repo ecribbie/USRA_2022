42e6f62f6521321522386f793b018c5c0d996439
Evan Cribbie <ecribbie@cedar1.cedar.computecanada.ca>
2022-06-07 10:04:58 -0700

    update

---
+++ YGOB/scripts/software_tests/macse_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar ../../../../TOOLS/macse_v2.06.jar -prog alignSequences -seq ../../Additional_files/NT.txt -out_NT macse_output_NT.fasta -out_AA macse_output_AA.fasta
---
+++ YGOB/scripts/software_tests/muscle_test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+muscle -in ../Additional_files/AA.txt -out muscle_test-out.txt
