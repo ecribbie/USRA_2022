ce59a8ecc81f0f81feefbcf8be8741aec8a14bb3
Evan Cribbie <ecribbie@cedar1.cedar.computecanada.ca>
2022-06-13 11:11:58 -0700

    update/6/13/11:11

--- YGOB/scripts/create_desired_pillar_file.py
+++ YGOB/scripts/create_desired_pillar_file.py
@@ -1,11 +1,15 @@
 import sys
+import functions as f
+
+species=
 
 pillars=f.pillar_filter(sys.argv[1],sys.argv[2])
+
 pillars={key: pillars[key]  for key in pillars.keys() if len(pillars[key]['genes'])>=3}
 
 families=f.pillar_resort(species,pillars)
 
-g=open('desired_pillar_file.txt','w')
+g=open('desired_pillar.txt','w')
 for fam in pillars:
     g.write(" ".join(pillars[fam]['genes']))
 
