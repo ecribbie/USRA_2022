15b98e0b8a8c4edfd2ebe654009e186b0e3f757b
Evan Cribbie <ecribbie@cedar5.cedar.computecanada.ca>
2022-06-14 12:31:20 -0700

    add run file while cleaning

---
+++ YGOB/DATA/run_get_ygob_fasta_file.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/AA.fsa' 'AA.fsa'
+
+
+python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/NT.fsa' 'NT.fsa'
+
+
+
+
