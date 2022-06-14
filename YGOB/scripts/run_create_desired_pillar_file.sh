cf7a36e6a1d994a0aa5894dee7b3503387892ce2
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-06-14 14:08:33 -0700

    Update run_create_desired_pillar_file.sh

--- YGOB/scripts/run_create_desired_pillar_file.sh
+++ YGOB/scripts/run_create_desired_pillar_file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../../../USRA_2022/YGOB/scripts/create_desired_pillar_file.py ../../../USRA_2022/YGOB/Additional_files/pillars.txt ../../../RAW_DATA/YGOB/AA.fsa ../../../USRA_2022/YGOB/scripts/gene_species_mapping_output.txt ../AA_SUMMARY/AA_genes.txt
+python create_desired_pillar_file.py ../DATA/pillars.txt ../DATA/AA.fsa ../DATA/gene_species_mapping_output.txt ../DATA/AA_genes.txt
