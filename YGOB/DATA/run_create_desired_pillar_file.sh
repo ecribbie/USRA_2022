d9ea081781b8699391343c10a01779bacbb01533
Evan Cribbie <ecribbie@cedar5.cedar.computecanada.ca>
2022-06-14 13:55:48 -0700

    move scripts to script folder

--- YGOB/DATA/run_create_desired_pillar_file.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ../../../USRA_2022/YGOB/scripts/create_desired_pillar_file.py ../../../USRA_2022/YGOB/Additional_files/pillars.txt ../../../RAW_DATA/YGOB/AA.fsa ../../../USRA_2022/YGOB/scripts/gene_species_mapping_output.txt ../AA_SUMMARY/AA_genes.txt
--- YGOB/DATA/run_get_family_fasta.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ../../../USRA_2022/YGOB/scripts/get_family_fasta.py ../../../RAW_DATA/YGOB/AA.txt ../DESIRED_PILLAR/desired_pillar.txt
