c4584eb653521d1beafc3c91948c16c7e398a610
ecribbie <104794022+ecribbie@users.noreply.github.com>
2022-06-14 14:06:13 -0700

    Update run_get_ygob_fasta_file.py

--- YGOB/scripts/run_get_ygob_fasta_file.py
+++ YGOB/scripts/run_get_ygob_fasta_file.py
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/AA.fsa' 'AA.fsa'
+python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/AA.fsa' '../DATA/AA.fsa'
 
 
-python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/NT.fsa' 'NT.fsa'
+python get_ygob_fasta_file.py 'http://ygob.ucd.ie/ygob/data/v7-Aug2012/NT.fsa' '../DATA/NT.fsa'
 
 
 
